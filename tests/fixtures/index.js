3b69a9074a53d70b02fc407baf02587b40fb6f66
Chang9601 <changsup96@gmail.com>
2023-04-02 11:32:50 +0900

    [43기 이창섭] ADD: 주문 추가 API 유닛 테스트

--- tests/fixtures/index.js
+++ tests/fixtures/index.js
@@ -1,7 +1,9 @@
 const usersFixture = require("./users-fixture");
 const itemsFixture = require("./items-fixture");
+const bidsFixture = require("./bids-fixture");
 
 module.exports = {
   usersFixture,
   itemsFixture,
+  bidsFixture
 };
--- tests/test-client.js
+++ tests/test-client.js
@@ -3,11 +3,68 @@ const dataSource = require("../api/models/dataSource");
 const truncateTables = async (tableList) => {
   await dataSource.query(`SET FOREIGN_KEY_CHECKS=0`);
 
-  for (let table of tableList) {
-    await dataSource.query(`TRUNCATE table ${table}`);
+  for (const table of tableList) {
+    await dataSource.query(`TRUNCATE ${table}`);
+    await dataSource.query(`ALTER TABLE ${table} AUTO_INCREMENT = 1`);
   }
 
   await dataSource.query(`SET FOREIGN_KEY_CHECKS=1`);
 };
 
 module.exports = { truncateTables };
+const createTestDataSet = async () => {
+  const orderStatus = [
+    [
+      1,
+      "결제완료"
+    ],
+    [     
+      2,
+      "상품준비"
+    ],
+    [
+      3,
+      "배송대기중"
+    ],
+    [
+      4,
+      "출고완료"
+    ],
+    [
+      5,
+      "배송중"
+    ],
+    [
+      6,
+      "배송완료"
+    ]
+  ];
+  
+  const shipment = [
+    [
+      1,
+      50000
+    ],
+    [
+      2,
+      100000
+    ],
+    [
+      3,
+      150000
+    ],
+    [
+      4,
+      200000
+    ],
+    [
+      5,
+      250000
+    ]
+  ];
+
+  await dataSource.query(`INSERT INTO order_status (id, name) VALUES ?`, [orderStatus]);
+  await dataSource.query(`INSERT INTO shipment (id, fee) VALUES ?`, [shipment]);
+}
+
+module.exports = { truncateTables, createTestDataSet };
