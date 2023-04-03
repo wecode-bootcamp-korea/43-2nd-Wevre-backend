63513ef767da038af992b3a99e36de1c528430a5
soohyun_kim5 <93cherry@naver.com>
2023-04-03 10:48:15 +0900

    [43기 김수현]ADD: 주문 페이지 조회 기능 구현

--- api/routes/orderRouter.js
+++ api/routes/orderRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const { orderController } = require("../controllers");
+const {loginRequired} = require("../utils/auth");
 
 const router = express.Router();
 
 router.post("", orderController.addOrder);
+router.get("/:itemId", loginRequired, orderController.getOrders);
 
 module.exports = router;
