b012e1c207f8b0830829e46eb8cef5f075936d5f
soohyun_kim5 <93cherry@naver.com>
2023-04-05 11:17:19 +0900

    [43기 김수현]상품리스트 조회 및 상품 등록

--- api/utils/imageUpload.js
+++ api/utils/imageUpload.js
@@ -1,22 +1,26 @@
 const AWS = require("aws-sdk")
 const multerS3 = require("multer-s3")
+const multer = require("multer")
 
 AWS.config.update({
-    region: 'ap-northeast-2',
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'ap-northeast-2',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 })
 
 const s3 = new AWS.S3();
 
-const imageUploader = multerS3({
-        s3: s3,
-        bucket: 'wevre/items',
-        key: (req, file, callback) => {
-            callback(null, `${Date.now()}_${file.originalname}`)
-        },
-        acl: 'public-read-write',
-        contentType: multerS3.AUTO_CONTENT_TYPE
-    })
+const imageUploader = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: "public-read",
+    bucket: 'wevre/test',
+    key:
+      (req, file, callback) => {
+        callback(null, `${Date.now()}_${file.originalname}`)
+      },
+    contentType: multerS3.AUTO_CONTENT_TYPE
+  })
+})
 
 module.exports = {imageUploader}
